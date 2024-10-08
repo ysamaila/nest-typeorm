e9446a1001a67fc2bb8362a7d3b271bfb6eb83eb
ysamaila <yusuf.hilside@gmail.com>
2024-10-08 07:58:48 -0700

    entitiy added

--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,6 +1,14 @@
-import { Entity } from "typeorm";
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity({name:'Todos'})
 export class Todo {
-    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({unique:true})
+    description: string;
+
+    @Column({type:'boolean', nullable:false})
+    completed: boolean;
+
 }
